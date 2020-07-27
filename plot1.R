b9724e0471c6b874e0f2a4cc8c12dec2e419ec45
Pablo Velásquez <pablovmurcia@yahoo.com>
2020-07-27 10:49:15 -0500

    plot 1 script

--- plot1.R
+++ plot1.R
@@ -28,7 +28,7 @@ class(household$datetime)
                                 # Plot 1 #
 ################################################################################
 png(filename="plot1.png", width=480, height=480,  type="cairo")
-par(mfrow = c(1,1),mar = c(4,4,2,2))
+par(mfrow = c(1,1),mar = c(6,4,2,2))
 hist(household$Global_active_power, xlab = "Global active power (kilowatt)",
      main ="Global active power", 
      col = "red",
